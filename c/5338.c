a5f8d5faa7782062966769b4bf778e31527440e2
guroprotector <sokkk6018@naver.com>
2020-08-16 22:50:28 +0900

    마이크로소프트 로고

---
+++ c/5338.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+	printf("        _.-;;-._\n");
+	printf(",_.._, |   ll   |\n");
+	printf(",_.._, |_.-;;-._|\n");
+	printf(",_.._, |   ll   |\n");
+	printf(",_.._, |_.-;;-._|\n");
+	return 0;
+}
