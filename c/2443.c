4983c4b393b1587c6232ae56aacae67a0c27942d
guroprotector <sokkk6018@naver.com>
2020-08-22 00:10:38 +0900

    별찍기-6

--- c/2443.c
+++ c/2443.c
@@ -2,16 +2,17 @@
 #include<stdio.h>
 int main()
 {
-	int k, s, a, b, c;
+	int k, s, a, b;
 	scanf_s("%d", &s);
 	k = 2*s - 1;
-	for (a = k; a > 0; a -=2)
+	for (a = 0; a <s; a ++)
 	{
-		for (b = a; b > 0; b--)
+
+		for (b = 0; b <k; b++)
 		{
-			//if (b )  이 if문을 어떻게 해야될지
- 				//printf(" ");
-			//else
+			if (b<a || b>=k-a)  //이 if문을 어떻게 해야될지
+				printf(" ");
+			else
 				printf("*");
 		}
 		printf("\n");
